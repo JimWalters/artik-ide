daafcb7203e9eb4ebbde2f2ab19373c1ea18eade
Alexander Garagatyi <agaragatyi@codenvy.com>
2016-04-27 15:00:55 +0300

    CHE-1059: workaround artik connection hanging (#28)

--- plugins/plugin-artik-server/src/main/java/org/eclipse/che/plugin/machine/artik/ArtikMachineTerminalLauncher.java
+++ plugins/plugin-artik-server/src/main/java/org/eclipse/che/plugin/machine/artik/ArtikMachineTerminalLauncher.java
@@ -11,21 +11,48 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.machine.artik;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import org.eclipse.che.api.machine.server.exception.MachineException;
+import org.eclipse.che.api.machine.server.spi.Instance;
 import org.eclipse.che.api.machine.server.terminal.WebsocketTerminalFilesPathProvider;
 import org.eclipse.che.plugin.machine.ssh.SshMachineImplTerminalLauncher;
+import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * author Alexander Garagatyi
  */
 public class ArtikMachineTerminalLauncher extends SshMachineImplTerminalLauncher {
+    private static final Logger LOG = getLogger(ArtikMachineTerminalLauncher.class);
+
+    private final ExecutorService executor;
+
     @Inject
     public ArtikMachineTerminalLauncher(@Named(TERMINAL_LAUNCH_COMMAND_PROPERTY) String runTerminalCommand,
                                         @Named(TERMINAL_LOCATION_PROPERTY) String terminalLocation,
                                         WebsocketTerminalFilesPathProvider terminalPathProvider) {
         super(runTerminalCommand, terminalLocation, terminalPathProvider);
+        this.executor = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("MachineTerminalLauncher-%d")
+                                                                                .setDaemon(true)
+                                                                                .build());
+    }
+
+    @Override
+    public void launchTerminal(Instance machine) throws MachineException {
+        executor.execute(() -> {
+            try {
+                super.launchTerminal(machine);
+            } catch (MachineException e) {
+                LOG.error(e.getLocalizedMessage(), e);
+            }
+        });
     }
 
     @Override
